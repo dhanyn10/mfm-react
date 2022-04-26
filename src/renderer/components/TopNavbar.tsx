0637ddb7bf9fbc8790ffce42c1dbbfb3249ca46a
dhanyn10 <dhanyn.dhan@gmail.com>
2022-04-26 11:03:14 +0700

    update navbar

--- src/renderer/components/TopNavbar.tsx
+++ src/renderer/components/TopNavbar.tsx
@@ -7,20 +7,17 @@ import {
 
 const TopNavbar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="info" expand="lg" className="mb-4">
         <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">MFM</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <Nav.Link href="#home">Github</Nav.Link>
+                <NavDropdown title="Help" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Issues</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Pull Requests</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Developer Tools</NavDropdown.Item>
                 </NavDropdown>
             </Nav>
             </Navbar.Collapse>
