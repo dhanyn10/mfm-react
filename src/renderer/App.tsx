9ddd97971caa1201bde00925c6c776c167350ccd
dhanyn10 <dhanyn.dhan@gmail.com>
2022-04-26 11:02:23 +0700

    container component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,9 @@
 // import icon from '../../assets/icon.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
+import { Container } from 'react-bootstrap'
 import Topnavbar from './components/TopNavbar'
+import InputFolder from './components/InputFolder';
 import ListFiles from './components/listFiles'
 import Footer from './components/Footer'
 
@@ -9,8 +11,11 @@ export default function App() {
     return (
         <div>
             <Topnavbar/>
-            <ListFiles/>
-            <Footer/>
+            <Container>
+                <InputFolder/>
+                <ListFiles/>
+                <Footer/>
+            </Container>
         </div>
     );
 }
