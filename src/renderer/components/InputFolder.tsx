a744dda543ae637da5530f7b59e359a03a6b9962
dhanyn10 <dhanyn.dhan@gmail.com>
2022-04-26 12:01:12 +0700

    update input folder

--- src/renderer/components/InputFolder.tsx
+++ src/renderer/components/InputFolder.tsx
@@ -1,19 +1,27 @@
 import {
-    InputGroup,
-    FormControl,
+    Row,
+    Col,
+    Form,
     Button
 } from 'react-bootstrap'
 
 const InputFolder = () => {
     return (
-        <InputGroup className="mb-3">
-            <FormControl
-            placeholder="Folder..."
-            />
-            <Button variant="btn btn-info text-white" id="browse">
-            Browse
-            </Button>
-        </InputGroup>
+            <Row className='mb-4'>
+                <Col xs="8">
+                    <Form.Control
+                        className='rounded-0'
+                        type="text"
+                        placeholder="Folder..."
+                        disabled
+                    />
+                </Col>
+                <Col xs="4">
+                    <div className="d-grid gap-2">
+                        <Button variant="info rounded-0">Browse</Button>
+                    </div>
+                </Col>
+            </Row>
     )
 }
 
