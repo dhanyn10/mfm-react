a0de067a71a643097c5adf583d3aac80a45ee56e
dhanyn10 <dhanyn.dhan@gmail.com>
2022-05-03 12:26:10 +0700

    toggle devtools

--- src/renderer/components/TopNavbar.tsx
+++ src/renderer/components/TopNavbar.tsx
@@ -4,26 +4,35 @@ import {
   Nav,
   NavDropdown
 } from 'react-bootstrap'
+import React from 'react'
+import { getCurrentWebContents } from '@electron/remote'
 
-const TopNavbar = () => {
-  return (
-    <Navbar bg="info" expand="lg" className="mb-4">
-      <Container>
-        <Navbar.Brand href="#home">MFM</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Github</Nav.Link>
-          <NavDropdown title="Help" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Issues</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Pull Requests</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Developer Tools</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
+export default class TopNavbar extends React.Component {
+  toggleDevTools = () => {
+    let currentWebContents = getCurrentWebContents()
+    if(currentWebContents.isDevToolsOpened() == false)
+      currentWebContents.openDevTools({mode: 'bottom'})
+    else
+      currentWebContents.closeDevTools()
+  }
+  render() {
+    return (
+      <Navbar bg="info" expand="lg" className="mb-4">
+        <Container>
+          <Navbar.Brand href="#home">MFM</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Github</Nav.Link>
+            <NavDropdown title="Help" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Issues</NavDropdown.Item>
+              <NavDropdown.Item href="#">Pull Requests</NavDropdown.Item>
+              <NavDropdown.Item onClick={this.toggleDevTools}>Developer Tools</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
 }
-
-export default TopNavbar
