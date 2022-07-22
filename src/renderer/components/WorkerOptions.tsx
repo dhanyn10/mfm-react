f49d14df10433d7f26635750aaa290830cba590f
dhanyn10 <dhanyn.dhan@gmail.com>
2022-07-22 16:36:27 +0700

    change navbar to card with buttons

--- src/renderer/components/WorkerOptions.tsx
+++ src/renderer/components/WorkerOptions.tsx
@@ -1,9 +1,8 @@
 import {
-  Nav,
-  Navbar,
-  Container,
   OverlayTrigger,
-  Popover
+  Popover,
+  Button,
+  Card
 } from 'react-bootstrap'
 import React from 'react'
 
@@ -24,43 +23,43 @@ export default class WorkerOptions extends React.Component {
       })
     }
   }
+  Rename()
+  {
+    console.log('rename')
+  }
   render() {
     if(this.state.listLength > 0)
     {
       return (
-        <Navbar bg="light" className='mt-4 mb-2 border'>
-          <Container>
-            <Navbar.Collapse>
-              <Nav>
-                <Nav.Link href="#">Rename</Nav.Link>
-                <OverlayTrigger
-                  placement="bottom-start"
-                  overlay={
-                    <Popover id="popover-insert">
-                      <Popover.Body>
-                        Bulk insert more characters to your files name
-                      </Popover.Body>
-                    </Popover>
-                  }
-                >
-                  <Nav.Link href="#">Insert</Nav.Link>
-                </OverlayTrigger>
-                <OverlayTrigger
-                  placement="bottom-start"
-                  overlay={
-                    <Popover id="popover-delete">
-                      <Popover.Body>
-                        Delete duplicated files or folders
-                      </Popover.Body>
-                    </Popover>
-                  }
-                >
-                  <Nav.Link href="#">Delete</Nav.Link>
-                </OverlayTrigger>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <Card className="mb-3 rounded-0">
+          <Card.Body>
+            <Button size="sm" variant="outline-info rounded-pill">Rename</Button>{' '}
+            <OverlayTrigger
+              placement="bottom-start"
+              overlay={
+                <Popover id="popover-insert">
+                  <Popover.Body>
+                    insert more characters to your files name
+                  </Popover.Body>
+                </Popover>
+              }
+            >
+              <Button size="sm" variant="outline-info rounded-pill">Insert</Button>
+            </OverlayTrigger>{' '}
+            <OverlayTrigger
+              placement="bottom-start"
+              overlay={
+                <Popover id="popover-delete">
+                  <Popover.Body>
+                    delete duplicated files
+                  </Popover.Body>
+                </Popover>
+              }
+            >
+              <Button size="sm" variant="outline-info rounded-pill">Delete</Button>
+            </OverlayTrigger>{' '}
+          </Card.Body>
+        </Card>
       )
     }
   }
