9ed82d4d9a6f67d6ee5b40141f2ff16c3ebc8d4e
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-20 02:00:47 +0700

    fix lint

--- src/renderer/scripts/Utils.ts
+++ src/renderer/scripts/Utils.ts
@@ -1,24 +1,22 @@
 export const Utils = {
-    
-    fulldirFunc: (dir: string) => {
+  fulldirFunc: (dir: string) => {
+    let fulldir = dir;
 
-        var fulldir = dir
-
-        //delete random string as refresh prefix
-        //random string generates is like xxxx...(16 char)_refresh_
-        if(fulldir.slice(16,25) == "_refresh_")
-            fulldir = fulldir.slice(25)
-        
-        // converting backward slash to forward slash
-        fulldir = fulldir.replace(/\\/g, "/")
-        var dirlength = fulldir.length
-
-        //insert forward slash at the end of directory name
-        var dirlastindex = fulldir.substring(dirlength-1, dirlength)
-        if(dirlastindex != "/")
-        {
-            fulldir += "/"
-        }
-        return fulldir
+    // delete random string as refresh prefix
+    // random string generates is like xxxx...(16 char)_refresh_
+    if (fulldir.slice(16, 25) === '_refresh_') {
+      fulldir = fulldir.slice(25);
     }
-}
+
+    // converting backward slash to forward slash
+    fulldir = fulldir.replace(/\\/g, '/');
+    const dirlength = fulldir.length;
+
+    // insert forward slash at the end of directory name
+    const dirlastindex = fulldir.substring(dirlength - 1, dirlength);
+    if (dirlastindex !== '/') {
+      fulldir += '/';
+    }
+    return fulldir;
+  },
+};
