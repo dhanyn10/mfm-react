90eaa23c35b4520c13a9c49c8379bb9299f21eb2
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-19 17:05:44 +0700

    fix lint

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,50 @@
 // import icon from '../../assets/icon.svg';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { Container } from 'react-bootstrap'
-import Topnavbar from './components/TopNavbar'
-import InputFolder from './components/InputFolder';
-import ListFiles from './components/listFiles'
-import WorkerOptions from './components/WorkerOptions'
-
+import { Container } from 'react-bootstrap';
 import React from 'react';
+import Topnavbar from './components/TopNavbar';
+import { InputFolder } from './components/InputFolder';
+import ListFiles from './components/ListFiles';
+import WorkerOptions from './components/WorkerOptions';
 
-export default class App extends React.Component <any, any> {
+export default class App extends React.Component<any, any> {
   constructor(props: any) {
-    super(props)
+    super(props);
 
     this.state = {
       filespathdata: null,
-      listFilesData: []
-    }
+      listFilesData: [],
+    };
   }
+
   handlePathData = (value: string) => {
     // let report = "app" + value
     this.setState({
-      filespathdata: value
-    })
-  }
+      filespathdata: value,
+    });
+  };
+
   handlerSendFilesData = (value: Array<any>) => {
-    console.log('App - filesdata: ', value)
+    console.log('App - filesdata: ', value);
     this.setState({
-      listFilesData: value
-    })
-  }
+      listFilesData: value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <Topnavbar/>
+        <Topnavbar />
         <Container>
-          <InputFolder filespath={this.handlePathData}/>
-          <WorkerOptions
-            workerListFilesData={this.state.listFilesData}
-          />
+          <InputFolder filespath={this.handlePathData} />
+          <WorkerOptions workerListFilesData={this.state.listFilesData} />
           <ListFiles
             retrievefilespath={this.state.filespathdata}
             sendFilesData={this.handlerSendFilesData}
           />
         </Container>
       </div>
-    );  
+    );
   }
 }
